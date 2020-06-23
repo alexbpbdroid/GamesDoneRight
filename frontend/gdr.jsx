2de528888c7cbf8a5c8e5dc917643c52fa01eee6
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-06-22 21:31:01 -0700

    test render working

---
+++ frontend/gdr.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+debugger
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to GDR.com</h1>, root);
+});
+
