93ea8adf95d00a75b1381a3a0f2a4b3774985826
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-06-23 23:49:13 -0700

    more auth css

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,25 +4,35 @@ import { Link } from 'react-router-dom';
 const NavBar = ({ currentUser, logout }) => {
   const defaultLinks = () => (
     <nav className="navbar">
+      
       <div className="middle-navbar-links">
-        <Link to="/">HOME</Link>
-        &nbsp;&nbsp;
-        <Link to="/">STORE</Link>
-        &nbsp;&nbsp;
-        <Link to="/">ABOUT</Link>
-        &nbsp;&nbsp;
-        <Link to="/">COMMUNITY</Link>
-        &nbsp;&nbsp;
-        <Link to="/">SUPPORT</Link>
-        &nbsp;&nbsp;
+        
+        <Link to="/"><img src={window.logoURL} className="logo-icon" /></Link>
+        <Link to="/">STORE <img src={window.arrowURL} className="arrow-icon" /></Link>
+        <Link to="/">ABOUT <img src={window.arrowURL} className="arrow-icon" /></Link>
+        <Link to="/">COMMUNITY <img src={window.arrowURL} className="arrow-icon" /></Link>
+        <Link to="/">SUPPORT <img src={window.arrowURL} className="arrow-icon" /></Link>
+                
         <div class="dropdown">
-        <Link to="/signup" className="sign-in-link">SIGN IN</Link>
-          <div class="dropdown-content" id="sign-in-drop">
-            
-            <p>GOG.com is a digital distribution platform – an online store with a curated selection of games, an optional gaming client giving you freedom of choice, and a vivid community of gamers. All of this born from a deeply rooted love for games, utmost care about customers, and a belief that you should own the things you buy.</p>
+          <Link to="/signup" className="sign-in-link">SIGN IN <img src={window.arrowURL} className="arrow-icon" /></Link>
+            <div class="dropdown-content" id="sign-in-drop">
+              <nav class="sign-in-drop-buttons">
+                <Link to="/"><button id="create-account-button">CREATE ACCOUNT</button></Link>
+                <Link to="/"><button id="sign-in-button">SIGN IN</button></Link>
+                
+              </nav>
+
+            <p id="gog-description"><strong>GOG.com is a digital distribution platform – an online store with a curated selection of games, an optional gaming client giving you freedom of choice, and a vivid community of gamers.</strong> All of this born from a deeply rooted love for games, utmost care about customers, and a belief that you should own the things you buy.</p>
           </div>
         </div>
+        
       </div>
+
+      <div className="cart-search">
+        <Link to="/"><img src={window.cartURL} className="cart-icon" /></Link>
+        <Link to="/"><img src={window.searchURL} className="search-icon" /></Link>
+      </div>
+      
     </nav>
   );
   const personalNavBar = () => (
