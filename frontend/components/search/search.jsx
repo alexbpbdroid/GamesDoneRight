05ce5088bca04b87ff91ce0a0459e6ba64961fb5
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-10 10:54:34 -0700

    search component functions

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -5,6 +5,14 @@ import { fetchGames } from "../../actions/game_actions";
 class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchField: '',
+      searching: false,
+      searchResults: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+    this.handleReset = this.handleReset.bind(this)
     
   }
 
@@ -12,7 +20,34 @@ class Search extends React.Component {
     this.props.fetchGames();
   }
 
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({ 
+      searchField: e.target.value,
+      searching: true
+    })
+  }
+
+  handleSearch(e) {
+    this.setState({
+      searchResults: this.props.games.filter((game) =>
+        game.title.toLowerCase().includes(this.state.searchField.toLowerCase())
+      )
+    })
+  }
+
+  handleReset(e) {
+    this.setState({ 
+      searchField: "",
+      searchResults: [],
+      searching: false
+    })
+  }
+
   render() {
+
+    let searchResults;
+
 
     return (
       <div></div>
