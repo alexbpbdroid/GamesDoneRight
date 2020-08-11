c7404556051372702a04dde18c5b721fb355811e
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-11 14:45:42 -0700

    create skeleton of wishlist page

---
+++ frontend/components/wishlist/wishlist_page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class WishlistPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default WishlistPage;
