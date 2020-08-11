ec11196eed0ef8fa199d2ad67878fa5e31ee0605
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-11 14:33:15 -0700

    add wishlists reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import reviewsReducer from './reviews_reducer'
 import usersReducer from './users_reducer'
 import gamesReducer from './games_reducer'
+import wishlistsReducer from './wishlists_reducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   games: gamesReducer,
   reviews: reviewsReducer,
+  wishlists: wishlistsReducer
 })
 
 export default entitiesReducer
