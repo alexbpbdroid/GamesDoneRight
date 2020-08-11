ad09020237ad1066c4db56b8ed2d14e1d5465ac7
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-11 11:09:34 -0700

    create wishlists controller

---
+++ app/controllers/api/wishlists_controller.rb
@@ -0,0 +1,44 @@
+class Api::WishlistsController < ApplicationController
+  def index
+    @wishlists = Wishlist.all
+    render :index
+  end
+
+  def show
+    @wishlist = Wishllist.find(params[:id])
+  end
+
+  def create
+    @wishlist = Wishlist.new(wishlist_params)
+    @wishlist.user_id = current_user.id 
+    if @wishlist.save
+      render :show
+    else
+      render json: @wishlist.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @wishlist = Wishlist.find(params[:id])
+    if @wishlist.update(wishlist_params)
+      render :show
+    else
+      render json @wishlist.errors.full_messages, status: 400
+    end
+  end
+
+  def destroy
+    @wishlist = Wishlist.find(params[:id])
+    if @wishlist
+      @wishlist.destroy
+    end
+    render :show
+  end
+
+  private
+
+  def wishlist_params
+    params.require(:wishlist).permit(:game_id, :saved)
+  end
+
+end
