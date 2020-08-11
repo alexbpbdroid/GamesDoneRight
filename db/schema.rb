3b0e987ee76cb5ea8e13d3cdadb56fa716bfe4c3
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-11 11:11:34 -0700

    misc files for wishlist migration, model, controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_232233) do
+ActiveRecord::Schema.define(version: 2020_08_11_175120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,15 @@ ActiveRecord::Schema.define(version: 2020_08_05_232233) do
     t.datetime "updated_at", null: false
     t.index ["session_token"], name: "index_users_on_session_token"
     t.index ["username"], name: "index_users_on_username"
+  end
+
+  create_table "wishlists", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "user_id", null: false
+    t.boolean "saved", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id", "user_id"], name: "index_wishlists_on_game_id_and_user_id", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
