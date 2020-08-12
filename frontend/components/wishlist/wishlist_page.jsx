b4f3a830c5e0ac1cc9a453ea67b66839e0c6ba75
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-12 15:07:39 -0700

    wishlist component work

--- frontend/components/wishlist/wishlist_page.jsx
+++ frontend/components/wishlist/wishlist_page.jsx
@@ -6,8 +6,8 @@ class WishlistPage extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchWishlists(this.props.currentUser.id);
     this.props.fetchGames();
+    this.props.fetchWishlists(this.props.currentUser.id);
   }
 
   deleteWishlist(wishlistId) {
@@ -26,23 +26,32 @@ class WishlistPage extends React.Component {
       .filter((game) => gameIds.includes(game.id))
 
     return (
-      <div className="wishlist">
-        WISHLISTED TITLES ({games.length})
-        <div className="wishlist-games">
-          {games.map((game) => (
-            <>
-              <div className="wishlist-single-game" key={game.id}>
-                {game.title}{game.price}
-              </div>
-              <div>
-                <button onClick={() => 
-                this.props.deleteWishlist(userWishlist.find((wishlist) => 
-                  wishlist.game_id === game.id).id)}
-                >
-                </button>
-              </div>
-            </>
-          ))}
+      <div className="wishlist-outer">
+        <div className="wishlist-inner">
+          <div className="wishlist-header">WISHLISTED TITLES ({games.length})</div>
+          <div className="wishlist-games">
+            {games.map((game) => (
+              <>
+                <div className="wishlist-single-game" key={game.id}>
+                  <div className="wishlist-thumbnail-title">
+                    <img className="wishlist-thumbnail" src={game.photoUrls[2]} alt="" />
+                    <div className="wishlist-title">{game.title}</div>
+                  </div>
+                  <div className="wishlist-delete-price">
+                    <button id="wishlist-delete-btn" onClick={() =>
+                    this.props.deleteWishlist(userWishlist.find((wishlist) =>
+                      wishlist.game_id === game.id).id)}
+                    >x
+                    </button>
+                    <div className="wishlist-price">$ {game.price}</div>
+                  </div>
+                </div>
+                <div>
+                  
+                </div>
+              </>
+            ))}
+          </div>
         </div>
       </div>
     )
