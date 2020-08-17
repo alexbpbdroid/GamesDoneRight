27da47dd82cc38c81108c1778704f61d574f45ad
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-17 11:25:21 -0700

    add cartItems reducer, create cart container

--- frontend/components/wishlist/wishlist_container.js
+++ frontend/components/wishlist/wishlist_container.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { fetchGames } from '../../actions/game_actions';
 import WishlistPage from './wishlist_page';
 import { fetchWishlists, deleteWishlist } from '../../actions/wishlist_actions';
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import reviewsReducer from './reviews_reducer'
 import usersReducer from './users_reducer'
 import gamesReducer from './games_reducer'
 import wishlistsReducer from './wishlists_reducer'
+import cartItemsReducer from './cart_items_reducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   games: gamesReducer,
   reviews: reviewsReducer,
-  wishlists: wishlistsReducer
+  wishlists: wishlistsReducer,
+  cartItems: cartItemsReducer
 })
 
 export default entitiesReducer
