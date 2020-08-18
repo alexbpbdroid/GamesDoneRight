fc00d4e1908b2b59e5d4b7898dd1af7963ab4369
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-18 12:51:10 -0700

    fix wishlist page layout / css

--- frontend/components/wishlist/wishlist_page.jsx
+++ frontend/components/wishlist/wishlist_page.jsx
@@ -42,22 +42,22 @@ class WishlistPage extends React.Component {
           <div className="wishlist-header">WISHLISTED TITLES ( {games.length} )</div>
           <div className="wishlist-games">
             {games.map((game) => (
-              <>
-                <Link to={`/games/${game.id}`} className="wishlist-single-game" key={game.id}>
+              <div className="wishlist-single-game-outer" key={game.id}>
+                <Link to={`/games/${game.id}`} className="wishlist-single-game">
                   <div className="wishlist-thumbnail-title">
                     <img className="wishlist-thumbnail" src={game.photoUrls[2]} alt="" />
                     <div className="wishlist-title">{game.title}</div>
-                  </div>
-                  <div className="wishlist-delete-price">
-                    <div className="wishlist-price">$ {game.price}</div>
-                  </div>
+                  </div>                 
                 </Link> 
-                <button id="wishlist-delete-btn" onClick={() =>
-                  this.props.deleteWishlist(userWishlist.find((wishlist) =>
-                    wishlist.game_id === game.id).id)}
-                >x
-                </button>
-              </>
+                <div id="wishlist-delete-price-id" className="wishlist-delete-price">
+                  <button id="wishlist-delete-btn" onClick={() =>
+                    this.props.deleteWishlist(userWishlist.find((wishlist) =>
+                      wishlist.game_id === game.id).id)}
+                  >x
+                  </button>
+                  <div className="wishlist-price">$ {game.price}</div>                 
+                </div>
+              </div>
             ))}
           </div>
         </div>
