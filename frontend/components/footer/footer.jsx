6bdabe6039258670f331a6d65507e3d157618b9d
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-20 11:34:28 -0700

    added favicon, changed footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,18 +4,17 @@ const GlobalFooter = () => (
   <div className='global-footer'>
     <div className='upper-footer'>
       <div className="upper-footer-content">
-        <div className="footer-icons">
-          <a href="https://github.com/alexbpbdroid"><i id="footer-git" className="fab fa-github-square"></i></a>
-          <a href="https://www.linkedin.com/in/alex-daniel-7389071b1/"><i id="footer-linkedin" className="fab fa-linkedin"></i></a>
-          <a href="http://www.alexjdaniel.com/"><i id="footer-personal" className="fas fa-user-circle"></i></a>
-        </div>
+        
       </div>
     </div>
 
     <div className='middle-footer'>
       <div className="middle-footer-content">
-        <div className="middle-footer-icon"><img id="middle-footer-icon" src={window.gdrLogoURL} alt="" /></div>&nbsp;&nbsp;&nbsp;
-        <div>Language: &nbsp;&nbsp;&nbsp; <span className="purple-text">English</span></div>
+        <div className="footer-icons">
+          <a href="https://github.com/alexbpbdroid"><i id="footer-git" className="fab fa-github-square"></i></a>
+          <a href="https://www.linkedin.com/in/alex-daniel-7389071b1/"><i id="footer-linkedin" className="fab fa-linkedin"></i></a>
+          <a href="http://www.alexjdaniel.com/"><i id="footer-personal" className="fas fa-user-circle"></i></a>
+        </div>
       </div>
     </div>
 
