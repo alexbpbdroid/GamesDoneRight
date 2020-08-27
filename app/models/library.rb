e1d99f90539d4464f7b18c42cc48a5d72a1f5948
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-26 22:30:54 -0700

    create model for library

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,10 @@
 class Library < ApplicationRecord
+  validates :user_id, uniqueness: {
+    scope: :game_id,
+    message: "Game is already in library"
+  }
+
+  belongs_to :user
+  
+  belongs_to :game
 end
