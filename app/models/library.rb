8737dec7a4dd69e8f2ca9234e7c6cc30641949b5
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-08-26 22:36:45 -0700

    add library routes, update library model

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,10 +1,10 @@
 class Library < ApplicationRecord
-  validates :user_id, uniqueness: {
-    scope: :game_id,
-    message: "Game is already in library"
-  }
+  validates :game_id, :user_id, presence: true
+  validates :saved, inclusion: {in: [true, false]}
 
   belongs_to :user
+    foreign_key: :user_id
   
-  belongs_to :game
+  belongs_to :game,
+    foreign_key: :game_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :reviews
     resources :wishlists, only: [:index, :show, :create, :update, :destroy]
     resources :carts, only: [:index, :show, :create, :update, :destroy]
+    resources :libraries, only: [:index, :show, :create, :update, :destroy]
   end  
 end
