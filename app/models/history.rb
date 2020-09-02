e1a05ab766d4a521a7db08586a12c3bd443619f7
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-09-01 23:23:14 -0700

    create model for order history

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,2 +1,10 @@
 class History < ApplicationRecord
+  validates :game_id, :user_id, presence: true
+  validates :saved, inclusion: {in: [true, false]}
+
+  belongs_to :user,
+    foreign_key: :user_id
+
+  belongs_to :game,
+    foreign_key: :game_id
 end
