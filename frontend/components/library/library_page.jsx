d58c5723c03422d2eea23d19509d8c99a363a5ec
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-09-02 22:51:05 -0700

    create initial library page skeleton

---
+++ frontend/components/library/library_page.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class LibraryPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchGames();
+    if(this.props.currentUser) this.props.fetchLibraries(this.props.currentUser.id);
+  }
+
+  render() {
+    if(!this.props.currentUser) {
+      return (
+        <div>* Must be logged in to view your library</div>
+      )
+    }
+
+    if (!this.props.games || !this.props.libraries) {
+      return null;
+    }
+  }
+}
+
+export default LibraryPage;
