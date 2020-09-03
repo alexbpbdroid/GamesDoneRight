22b7f5f8c834844e2db7040ce87c0826f95721eb
alexbpbdroid@gmail.com <alexbpbdroid@gmail.com>
2020-09-02 23:11:09 -0700

    list purchased games on order history page

--- frontend/components/history/history_page.jsx
+++ frontend/components/history/history_page.jsx
@@ -28,6 +28,17 @@ class HistoryPage extends React.Component {
     let gameIds = userHistory.map((history) => history.game_id)
     let games = Object.values(this.props.games)
       .filter((game) => gameIds.includes(game.id))
+
+    return (
+      <div className="history-outer">
+        <div className="history-header">Here's your order history:</div>
+        <div className="history-games">
+          {games.map((game) => (
+            <div>{game.title}</div>
+          ))}
+        </div>
+      </div>
+    )
   }
 }
 
